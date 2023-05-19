3c5a67eb3116935753b84c8dfe4ce0e3d8528881
hcuevasr <hcuevasr@utem.cl>
2023-05-18 23:00:30 -0400

    primer commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Configura la conexión a la base de datos MySQL
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Establece la conexión a la base de datos
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos MySQL');
+});
+
+// Obtener todos los videos
+app.get('/videos', (req, res) => {
+  const sql = 'SELECT * FROM videos';
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error al obtener los videos:', err);
+      res.status(500).json({ error: 'Error al obtener los videos' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+// Obtener un video específico
+app.get('/videos/:id', (req, res) => {
+  const videoId = req.params.id;
+  const sql = 'SELECT * FROM videos WHERE id = ?';
+  connection.query(sql, [videoId], (err, results) => {
+    if (err) {
+      console.error('Error al obtener el video:', err);
+      res.status(500).json({ error: 'Error al obtener el video' });
+      return;
+    }
+    if (results.length === 0) {
+      res.status(404).json({ error: 'Video no encontrado' });
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+
+// Agregar un nuevo video
+app.post('/videos', (req, res) => {
+  const { url, title, description, duration, thumbnail } = req.body;
+  const sql = 'INSERT INTO videos (url, title, description, duration, thumbnail) VALUES (?, ?, ?, ?, ?)';
+  const values = [url, title, description, duration, thumbnail];
+  connection.query(sql, values, (err, results) => {
+    if (err) {
+      console.error('Error al agregar el video:', err);
+      res.status(500).json({ error: 'Error al agregar el video' });
+      return;
+    }
+    res.status(201).json({ message: 'Video agregado exitosamente' });
+  });
+});
+
+// Eliminar un video
+app.delete('/videos/:id', (req, res) => {
+  const videoId = req.params.id;
+  const sql = 'DELETE FROM videos WHERE id = ?';
+  connection.query(sql, [videoId], (err, results) => {
+    if (err) {
+      console.error('Error al eliminar el video:', err);
+      res.status(500).json({ error: 'Error al eliminar el video' });
+      return;
+    }
+    res.json({ message: 'Video eliminado exitosamente' });
+  });
+});
+
+// Inicia el servidor
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
