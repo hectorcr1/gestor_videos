352c524e77d1e4c43e1141e8246bcd5b06494d40
hcuevasr <hcuevasr@utem.cl>
2023-05-21 23:23:21 -0400

    se cambia codigicacion de la bs a utf8mb4 por problemas con emojis

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  charset: 'utf8mb4'
 });
 
 // Establece la conexión a la base de datos
